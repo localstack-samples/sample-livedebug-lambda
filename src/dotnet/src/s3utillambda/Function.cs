23e196489192978e2a18aee478fd141620604823
Chad Beaulac <chad.beaulac@localstack.cloud>
2023-07-25 13:27:37 -0700

    Updated code and README to use AWS_ENDPOINT_URL and AWS_ENDPOINT_URL_S3 and not use special S3Client inside Lambda

--- src/dotnet/src/s3utillambda/Function.cs
+++ src/dotnet/src/s3utillambda/Function.cs
@@ -13,24 +13,12 @@ namespace s3utillambda;
 public class Function
 {
 
-    private const string IsIdeDevVariableName = "IS_IDE_DEV";
     private const string BucketVariableName = "BUCKET";
 
-    private AmazonS3Client GetS3Client(string isIdeDev, ILambdaContext context) {
-        if (isIdeDev == "1")
-        {
-            context.Logger.Log($"IDE Dev");
-            AmazonS3Config config = new AmazonS3Config();
-            config.ServiceURL = "http://host.docker.internal:4566";
-            config.ForcePathStyle = true;
-
-            return new AmazonS3Client(new BasicAWSCredentials("test", "test"), config);
-        }
-        else
-        {
-            return new AmazonS3Client();
-        }
+    private AmazonS3Client GetS3Client(ILambdaContext context) {
+        return new AmazonS3Client();
     }
+
 
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
@@ -42,10 +30,9 @@ public class Function
     {
 
         // Check the environment variable
-        string isIdeDev = Environment.GetEnvironmentVariable(IsIdeDevVariableName);
         string bucketName = Environment.GetEnvironmentVariable(BucketVariableName);
         context.Logger.Log($"bucketName {bucketName}\n");
-        AmazonS3Client _s3Client = GetS3Client(isIdeDev, context);
+        AmazonS3Client _s3Client = GetS3Client(context);
         context.Logger.Log($"Testing 1 2 3\n");
         var listObjectsRequest = new ListObjectsRequest
         {
